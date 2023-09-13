4fc9a53ee82b4e8f40f38c1ad3ec7f482ea4116e
rkc137 <hlopov100@gmail.com>
2023-09-13 21:33:59 +0300

    begin stuff

--- main.cpp
+++ main.cpp
@@ -5,18 +5,24 @@
 class obj : public sf::RectangleShape
 {
 public:
-    obj(int v) : sf::RectangleShape(sf::Vector2f(objs_width, objs_height * v)), value(v)
+    obj(int v) : sf::RectangleShape(sf::Vector2f(objs_width, -(objs_height * v))), value(v)
     {
-        setPosition(sf::Vector2f(v * objs_width, 0));
         setFillColor(sf::Color(v * 10, 100, 100));
     }
+    
+    void set_pos(int x)
+    {
+        setPosition(sf::Vector2f(x * objs_width, window_height));
+    }
+    
     static void set_shapes(int objs_count, int width, int height)
     {
+        window_height = height;
         objs_width = width / objs_count;
         objs_height = height / objs_count;
     }
 private:
-    inline static int objs_width = 10, objs_height = 10;
+    inline static int objs_width, objs_height, window_height;
     int value;
 };
 
@@ -24,14 +30,16 @@ int main()
 {
     sf::RenderWindow window(sf::VideoMode(800, 600), "graph");
 
-    int objs_count = 10;
+    int objs_count = 50;
     obj::set_shapes(objs_count, window.getSize().x, window.getSize().y);
 
     std::vector<obj> vec;
     vec.reserve(objs_count);
-    for(int i = 0; i < objs_count; i++)
+    for(int i = 1; i <= objs_count; i++)
         vec.emplace_back(i);
-
+    std::random_shuffle(vec.begin(), vec.end());
+    for(int i = 0; i < objs_count; i++)
+        vec[i].set_pos(i);
 
     while(window.isOpen())
     {
@@ -44,7 +52,7 @@ int main()
                 window.close();
                 return 0;
             case sf::Event::Resized:
-                
+                break;
             default:
                 break;
             }
